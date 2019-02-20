0a208ad58760cdaab774c16a270ffb39efc990a5
Daniel <d.p.davison@utwente.nl>
2019-02-20 15:36:40 +0100

    debugged with Carlos -- now is a working version of the UT report generator script

--- python/ut_reporting.py
+++ python/ut_reporting.py
@@ -1,3 +1,5 @@
+#! /usr/bin/env python
+
 import rosbag
 import json
 import re
@@ -6,12 +8,12 @@ import re
 def ut_report_activities(ts):
     #regular expression patterns for the various message formats that we are interested in
     reStartActivity = re.compile(r"Adult pressed button: \[start_([a-zA-Z]+)\]")
-    reCorrect = re.compile(r"Child gave correct answer\. .*")
-    reIncorrect = re.compile(r"Child gave incorrect answer\. .*")
+    reCorrect = re.compile(r"Child gave correct answer\. (.)*")
+    reIncorrect = re.compile(r"Child gave incorrect answer\. (.)*")
     reEndActivity = re.compile(r"Adult pressed button: \[end_([a-zA-Z]+)\]")
 
     #the topic where our logger writes each log message
-    topic = ['ut_logger']
+    topic_list = ['/ut_logger']
     
     #the dict file that we fill, this will go into the report
     utDict = {
@@ -19,22 +21,23 @@ def ut_report_activities(ts):
             'ExploreFeatures':{'count':0,'total_duration':0,'correct':0,'incorrect':0},
             'PromptFeatures':{'count':0,'total_duration':0,'correct':0,'incorrect':0},
             'ExploreExpressions':{'count':0,'total_duration':0,'correct':0,'incorrect':0},
-            'PromptExpressions':{'count':0,'total_duration':0,'correct':0,'incorrect':0}
+            'PromptExpressions':{'count':0,'total_duration':0,'correct':0,'incorrect':0},
+            'Greetings':{'count':0,'total_duration':0,'correct':0,'incorrect':0}
         }
     
     #open the bag file where the stream of ROS messages is stored
     #TODO: in case the filename does not match exactly the timestamp, search for the closest "xxx.bag" file
-    bag = rosbag.Bag(str(ts)+'.bag')
-    
+    bag = rosbag.Bag(str(ts)+'session.bag')
+    activity = 'none'
+
     #start parsing each message, searching for the specific log messages that we need to report
-    for topic, msg, t in bag.read_messages(topics=[topic]):
+    for topic, msg, t in bag.read_messages(topics=topic_list):
         #message should be JSON string, so try to parse
         try:
-            jmsg = json.loads(msg.message)
+            jmsg = json.loads(msg.data)
+            # print jmsg
         except ValueError:
             print("Oops, the message is not in JSON string format... "+str(msg))
-            
-        activity = 'none'
         
         #search for starting an activity, when found we increase the counter and save the start timestamp
         searchStartActivity = reStartActivity.search(jmsg['message'])
@@ -42,8 +45,9 @@ def ut_report_activities(ts):
             if activity != 'none':
                 print("That's strange, a new activity has started before the previous ended...")
             activity = searchStartActivity.group(1)
-            actStartTime = jmsg['timestamp']
+            actStartTime = int(jmsg['timestamp'])
             utDict[activity]['count'] = utDict[activity].get('count', 0) + 1
+            # print "Start of activity - {} at {}".format(activity, actStartTime)
         
         searchCorrect = reCorrect.search(jmsg['message'])
         if searchCorrect and activity != 'none':
@@ -56,8 +60,9 @@ def ut_report_activities(ts):
         #search for ending an activity, we then calculate the duration that was spent in this activity
         searchEndActivity = reEndActivity.search(jmsg['message'])
         if searchEndActivity and activity == searchEndActivity.group(1):
-            duration = jmsg['timestamp'] - actStartTime
+            duration = int(jmsg['timestamp']) - actStartTime
             utDict[activity]['total_duration'] = utDict[activity].get('total_duration', 0) + duration
+            # print "End of activity - {} from {} to {}".format(activity, actStartTime, jmsg['timestamp'])
             activity = 'none'
             
     return utDict
